4cad5448a0b71604491ce0a61e4b9d3cc425ca9d
GilHaham <hahamgil@gmail.com>
2019-01-08 19:07:00 +0200

    0

---
+++ Searcher.h
@@ -0,0 +1,16 @@
+//
+// Created by gil on 08/01/19.
+//
+
+#ifndef EX4_SEARCHER_H
+#define EX4_SEARCHER_H
+
+
+#include "ISearcher.h"
+
+class Searcher : public ISearcher {
+
+};
+
+
+#endif //EX4_SEARCHER_H
