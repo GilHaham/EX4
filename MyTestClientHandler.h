2b729ac5e0611afe625f7512a223bffa748712d9
meirco <meirco26@gmail.com>
2019-01-10 14:08:47 +0200

    0

--- MyTestClientHandler.h
+++ MyTestClientHandler.h
@@ -5,14 +5,11 @@
 #ifndef EX4_MYTESTCLIENTHANDLER_H
 #define EX4_MYTESTCLIENTHANDLER_H
 
+
 #include "ClientHandler.h"
 
 class MyTestClientHandler: public ClientHandler {
 
-    void handleClient(int socket)  {
-
-
-    }
 };
 
 
